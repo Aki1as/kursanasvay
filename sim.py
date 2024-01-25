b0bf8a2adbd3cdf0235b4b2f1a530d9f4a264d34
Aki1as <fantasticflowsize@gmail.com>
2024-01-25 07:42:57 +0300

    asas

--- sim.py
+++ sim.py
@@ -1,11 +1,26 @@
-from sympy import Matrix, symbols, Eq, solve
+# from sympy import Matrix, symbols, Eq, solve
 
-x, y = symbols('x y')
+# x, y = symbols('x y')
 
-A = Matrix([[1, 2], [3, 4]])
-B = Matrix([5, 6])
+# A = Matrix([[1, 2], [3, 4]])
+# B = Matrix([5, 6])
 
-система = Eq(A * Matrix([x, y]), B)
-решение = solve(система, (x, y))
+# система = Eq(A * Matrix([x, y]), B)
+# решение = solve(система, (x, y))
 
-print(решение)
+# print(решение)
+
+
+
+# from sympy import Matrix
+
+# A = Matrix([[1, 2], [3, 4]])
+# B = Matrix([[5, 6], [7, 8]])
+
+# # Произведение матриц
+# result_matrix = A * B
+# print("Произведение матриц:", result_matrix)
+
+# # Обратная матрица
+# inverse_matrix = A.inv()
+# print("Обратная матрица:", inverse_matrix)
